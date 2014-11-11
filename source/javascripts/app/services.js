a9e082905a8d2ffa2f576e7aadbaf116eb090f57
Tiago Devezas <tiagodevezas@gmail.com>
2014-11-11 09:34:54 +0000

    add sublime text project files to gitignore

--- source/javascripts/app/services.js
+++ source/javascripts/app/services.js
@@ -2,16 +2,18 @@
 
 var mediavizServices = angular.module('mediavizServices', ['ngResource']);
 
-mediavizServices.factory('Source', ['$resource',
-	function($resource) {
-		return $resource('http://mediaviz.fe.up.pt:3000/api/sources', {},
-		{
-			get: { 
-				method: 'JSONP',
-				isArray: true, 
-				params: {
-					callback: 'JSON_CALLBACK'
-				} 
-			}
-		});
-	}]);
+mediavizServices
+	.factory('Source', ['$resource',
+		function($resource) {
+			var baseUrl = 'http://mediaviz.fe.up.pt:3000/'
+			return $resource(baseUrl + 'api/sources', {},
+			{
+				get: { 
+					method: 'JSONP',
+					isArray: true, 
+					params: {
+						callback: 'JSON_CALLBACK'
+					} 
+				}
+			});
+		}]);
