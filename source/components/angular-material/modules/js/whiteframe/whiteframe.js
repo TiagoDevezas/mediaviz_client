2c6f3e8d0dc62c2d7e3d37f773a3fc99ff031f33
Tiago Devezas <tiagodevezas@gmail.com>
2015-04-14 18:00:29 +0100

    install angular-material through bower

---
+++ source/components/angular-material/modules/js/whiteframe/whiteframe.js
@@ -0,0 +1,15 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.8.3-master-554716f
+ */
+(function() {
+'use strict';
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+angular.module('material.components.whiteframe', []);
+})();
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,7 +2,9 @@
 //= require angular-route/angular-route.min
 //= require angular-resource/angular-resource.min
 //= require angular-animate/angular-animate.min
+//= require angular-aria/angular-aria.min
 //= require angular-sanitize/angular-sanitize.min
+//= require angular-material/angular-material.min
 //= require d3/d3.min
 //= require c3/c3.min
 //= require d3/colorbrewer
