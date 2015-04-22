b78bff3623e5538d3764d16ef5658bc55376ed4a
Tiago Devezas <tiagodevezas@gmail.com>
2015-04-22 13:57:41 +0100

    update angular-material

--- source/components/angular-material/modules/closure/sidenav/sidenav.js
+++ source/components/angular-material/modules/closure/sidenav/sidenav.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v0.9.0-rc1-master-55fa76a
+ * v0.9.0-rc2-master-bed14b9
  */
 goog.provide('ng.material.components.sidenav');
 goog.require('ng.material.components.backdrop');
