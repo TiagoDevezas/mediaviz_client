90ccc5a9fc95625ccfc7253383dae8a0c237d9c1
tiagodevezas <tiagodevezas@gmail.com>
2015-07-08 20:24:53 +0100

    start new NewsMap version

--- source/javascripts/app/controllers/controller.newsmap.js
+++ source/javascripts/app/controllers/controller.newsmap.js
@@ -1,7 +1,31 @@
-mediavizControllers.controller('NewsMapCtrl', function($scope, Resource, Page) {
+mediavizControllers.controller('NewsMapCtrl', function($scope, Resources, Page) {
 
   Page.setTitle('NewsMap');
 
+  $scope.defaultSource = null;
 
+  $scope.numMaps = 1;
+
+  $scope.visibleMaps = [
+    {name: 'map1', visible: true},
+    {name: 'map2', visible: true}
+  ];
+
+  $scope.appendMap = function() {
+    if($scope.numMaps === 1) $scope.numMaps += 1;
+  }
+
+  $scope.paramsObj = {resource: 'places', since: null, until: null, type: 'national', q: null, lang: 'pt', map: 'world'};
+
+  // getMapData();
+
+  function getMapData() {
+    $scope.loading = true;
+    Resources.get($scope.paramsObj).$promise.then(function(data) {
+      $scope.loading = false;
+      // $scope.mapData = data;
+      // console.log($scope.mapData);
+    });
+  }
 
 });
