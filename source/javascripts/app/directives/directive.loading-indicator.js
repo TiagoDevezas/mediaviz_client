76d8ca71887ac1d281baa5f9f0af3541553fb2a2
tiagodevezas <tiagodevezas@gmail.com>
2015-07-15 16:40:49 +0100

    fix firefox quirk with flexbox and svg

--- source/javascripts/app/directives/directive.loading-indicator.js
+++ source/javascripts/app/directives/directive.loading-indicator.js
@@ -2,6 +2,6 @@ mediavizDirectives.directive('loadingFlash', function() {
  return {
    restrict: 'AE',
    transclude: true,
-   template: '<div ng-show="loading" layout="column" layout-margin><md-progress-linear md-mode="indeterminate"></md-progress-linear></div>'
+   template: '<div ng-show="loading" style="position: absolute; top: -5px; left: 0; right: 0;"><md-progress-linear md-mode="indeterminate" class="md-accent"></md-progress-linear></div>'
  }
 });
