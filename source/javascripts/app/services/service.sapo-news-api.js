2985fb601fb9f07b873ae4e0a3965a4002b2a5b2
Tiago Devezas <tiagodevezas@gmail.com>
2015-07-27 10:56:17 +0100

    add json callback to SAPO api call

--- source/javascripts/app/services/service.sapo-news-api.js
+++ source/javascripts/app/services/service.sapo-news-api.js
@@ -1,8 +1,9 @@
 mediavizServices.factory('SAPONews', function($http) {
-  var baseUrl = "http://services.sapo.pt/InformationRetrieval/Epicentro/GetTrendline"
+  var baseUrl = "http://services.sapo.pt/InformationRetrieval/Epicentro/GetTrendline";
+  var callback = '?callback=JSON_CALLBACK';
   var factory = {};
   factory.get = function(options) {
-    return $http.get(baseUrl, {params: options, cache: false}).then(function(response) {
+    return $http.jsonp(baseUrl + callback, {params: options, cache: false}).then(function(response) {
       return response;
     });
   }
