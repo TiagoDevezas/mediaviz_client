7d61c030468560c6fbd22478d8ed0b0ce5f8e2ed
tiagodevezas <tiagodevezas@gmail.com>
2015-09-23 14:22:31 +0100

    update angular material

--- source/components/angular-material/modules/closure/datepicker/datepicker.js
+++ source/components/angular-material/modules/closure/datepicker/datepicker.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v0.11.0-master-4a16038
+ * v0.11.0-master-bc4100a
  */
 goog.provide('ng.material.components.datepicker');
 goog.require('ng.material.components.icon');
@@ -870,7 +870,8 @@ goog.require('ng.material.core');
    *
    *     // Example uses moment.js to parse and format dates.
    *     $mdDateLocaleProvider.parseDate = function(dateString) {
-   *       return moment(dateString).toDate();
+   *       var m = moment(dateString, 'L', true);
+   *       return m.isValid() ? m.toDate() : new Date(NaN);
    *     };
    *
    *     $mdDateLocaleProvider.formatDate = function(date) {
@@ -1418,9 +1419,7 @@ goog.require('ng.material.core');
     this.calendarButton.disabled = isDisabled;
   };
 
-  /**
-   * Resizes the input element based on the size of its content.
-   */
+  /** Resizes the input element based on the size of its content. */
   DatePickerCtrl.prototype.resizeInputElement = function() {
     this.inputElement.size = this.inputElement.value.length + EXTRA_INPUT_SIZE;
   };
@@ -1453,6 +1452,7 @@ goog.require('ng.material.core');
   /** Position and attach the floating calendar to the document. */
   DatePickerCtrl.prototype.attachCalendarPane = function() {
     var calendarPane = this.calendarPane;
+    calendarPane.style.transform = '';
     this.$element.addClass('md-datepicker-open');
 
     var elementRect = this.inputContainer.getBoundingClientRect();
@@ -1464,7 +1464,22 @@ goog.require('ng.material.core');
     var paneLeft = elementRect.left - bodyRect.left;
 
     // If the right edge of the pane would be off the screen and shifting it left by the
-    // difference would not go past the left edge of the screen.
+    // difference would not go past the left edge of the screen. If the calendar pane is too
+    // big to fit on the screen at all, move it to the left of the screen and scale the entire
+    // element down to fit.
+    if (paneLeft + CALENDAR_PANE_WIDTH > bodyRect.right) {
+      if (bodyRect.right - CALENDAR_PANE_WIDTH > 0) {
+        paneLeft = bodyRect.right - CALENDAR_PANE_WIDTH;
+      } else {
+        paneLeft = 0;
+        var scale = bodyRect.width / CALENDAR_PANE_WIDTH;
+        calendarPane.style.transform = 'scale(' + scale + ')';
+      }
+
+      calendarPane.classList.add('md-datepicker-pos-adjusted');
+    }
+
+
     if (paneLeft + CALENDAR_PANE_WIDTH > bodyRect.right &&
         bodyRect.right - CALENDAR_PANE_WIDTH > 0) {
       paneLeft = bodyRect.right - CALENDAR_PANE_WIDTH;
@@ -1481,7 +1496,7 @@ goog.require('ng.material.core');
 
     calendarPane.style.left = paneLeft + 'px';
     calendarPane.style.top = paneTop + 'px';
-    document.body.appendChild(this.calendarPane);
+    document.body.appendChild(calendarPane);
 
     // The top of the calendar pane is a transparent box that shows the text input underneath.
     // Since the pane is floating, though, the page underneath the pane *adjacent* to the input is
@@ -1786,7 +1801,9 @@ goog.require('ng.material.core');
      * @param {Date} date
      */
     function setDateTimeToMidnight(date) {
-      date.setHours(0, 0, 0, 0);
+      if (isValidDate(date)) {
+        date.setHours(0, 0, 0, 0);
+      }
     }
 
     /**
--- source/components/angular-material/modules/js/datepicker/datepicker.js
+++ source/components/angular-material/modules/js/datepicker/datepicker.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v0.11.0-master-4a16038
+ * v0.11.0-master-bc4100a
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -869,7 +869,8 @@
    *
    *     // Example uses moment.js to parse and format dates.
    *     $mdDateLocaleProvider.parseDate = function(dateString) {
-   *       return moment(dateString).toDate();
+   *       var m = moment(dateString, 'L', true);
+   *       return m.isValid() ? m.toDate() : new Date(NaN);
    *     };
    *
    *     $mdDateLocaleProvider.formatDate = function(date) {
@@ -1417,9 +1418,7 @@
     this.calendarButton.disabled = isDisabled;
   };
 
-  /**
-   * Resizes the input element based on the size of its content.
-   */
+  /** Resizes the input element based on the size of its content. */
   DatePickerCtrl.prototype.resizeInputElement = function() {
     this.inputElement.size = this.inputElement.value.length + EXTRA_INPUT_SIZE;
   };
@@ -1452,6 +1451,7 @@
   /** Position and attach the floating calendar to the document. */
   DatePickerCtrl.prototype.attachCalendarPane = function() {
     var calendarPane = this.calendarPane;
+    calendarPane.style.transform = '';
     this.$element.addClass('md-datepicker-open');
 
     var elementRect = this.inputContainer.getBoundingClientRect();
@@ -1463,7 +1463,22 @@
     var paneLeft = elementRect.left - bodyRect.left;
 
     // If the right edge of the pane would be off the screen and shifting it left by the
-    // difference would not go past the left edge of the screen.
+    // difference would not go past the left edge of the screen. If the calendar pane is too
+    // big to fit on the screen at all, move it to the left of the screen and scale the entire
+    // element down to fit.
+    if (paneLeft + CALENDAR_PANE_WIDTH > bodyRect.right) {
+      if (bodyRect.right - CALENDAR_PANE_WIDTH > 0) {
+        paneLeft = bodyRect.right - CALENDAR_PANE_WIDTH;
+      } else {
+        paneLeft = 0;
+        var scale = bodyRect.width / CALENDAR_PANE_WIDTH;
+        calendarPane.style.transform = 'scale(' + scale + ')';
+      }
+
+      calendarPane.classList.add('md-datepicker-pos-adjusted');
+    }
+
+
     if (paneLeft + CALENDAR_PANE_WIDTH > bodyRect.right &&
         bodyRect.right - CALENDAR_PANE_WIDTH > 0) {
       paneLeft = bodyRect.right - CALENDAR_PANE_WIDTH;
@@ -1480,7 +1495,7 @@
 
     calendarPane.style.left = paneLeft + 'px';
     calendarPane.style.top = paneTop + 'px';
-    document.body.appendChild(this.calendarPane);
+    document.body.appendChild(calendarPane);
 
     // The top of the calendar pane is a transparent box that shows the text input underneath.
     // Since the pane is floating, though, the page underneath the pane *adjacent* to the input is
@@ -1785,7 +1800,9 @@
      * @param {Date} date
      */
     function setDateTimeToMidnight(date) {
-      date.setHours(0, 0, 0, 0);
+      if (isValidDate(date)) {
+        date.setHours(0, 0, 0, 0);
+      }
     }
 
     /**
